bfae1218a49257cf5c6b0a05315ea187ae2b7795
Steve <cordovas@bc.edu>
2018-12-19 23:49:57 -0500

    update

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessageController extends Controller
 {
@@ -15,23 +16,59 @@ class MessageController extends Controller
     public function create(Request $request)
     {
         return view('create');
+
+//        $hobbies = Hobby::getForCheckboxes();
+//        return view('crm.create')->with([
+//            'hobbies' => $hobbies,
+//        ]);
     }
 
     public function store(Request $request)
     {
-//        dump($request->all());
-
-        $name = ($request->input('name'));
-        $location = ($request->input('location'));
-        $story = ($request->input('story'));
-
-        return redirect('/messages/create')->with([
-            'name' => $name,
-            'location' => $location,
-            'story' => $story
+        # Validate the request data
+        $request->validate([
+            'name' => 'required|alphaNumeric',
+            'location' =>  'required|alphaNumeric',
+            'story'=>'required|alphaNumeric',
         ]);
-//        return view('create');
+        $message = new Message();
+        $message->name = $request->name;
+        $message->location = $request->location;
+        $message->story = $request->story;
+
+        $message->save();
+        return redirect('/message')->with([
+            'alert' => 'Your message was added.'
+        ]);
+
+
+//        return redirect('/messages/create')->with([
+//            'name' => $name,
+//            'location' => $location,
+//            'story' => $story
+//        ]);
+
+
     }
+
+    public function practiceX()
+    {
+        # Instantiate a new Message Model object
+        $message = new Message();
+
+        # Set the properties
+        $message->name = 'Harry Potter and the Sorcerer\'s Stone';
+        $message->location = 'Newark';
+        $message->story = 'It works!';
+
+        # Invoke the Eloquent `save` method to generate a new row in the
+        # `books` table, with the above data
+        $message->save();
+
+        dump('Added: '.$message->name);
+    }
+
+
 
 //    public function index(Request $request)
 //    {
--- routes/web.php
+++ routes/web.php
@@ -30,24 +30,20 @@ Route::get('/debug', function () {
 
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*
  * Home
  */
 Route::get('/', 'WelcomeController');
 
+
+/*
+ * Add Testimonials
+ */
 Route::get('/messages/create', 'MessageController@create');
 
 Route::get('/messages', 'MessageController@store');
+
+Route::get('/test2', 'MessageController@practiceX');
+
 
 
 
